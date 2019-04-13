f6c3983f910bc60d3213a4d164854b9d240ec740
jus0k <jutjd115@gmail.com>
2019-04-13 12:56:20 +0900

    Add Game componant

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import styles from './App.module.css';
-import { Heading } from './components';
+import { Heading, Game } from './components';
 
 class App extends Component {
   render() {
     return (
       <div className={styles.wrapper}>
         <Heading />
+        <Game />
       </div>
     );
   }
---
+++ src/components/Game/Game.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Game.module.css';
+
+const Game = () => (
+  <div className={styles.wrapper}>
+    Game
+  </div>
+)
+
+export default Game;
